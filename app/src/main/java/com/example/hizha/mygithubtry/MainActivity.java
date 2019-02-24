812eac423915726931647e41cc88de7d0a1c0e5e
milo <milo@163.com>
2019-02-24 09:51:18 +0800

    first

---
+++ app/src/main/java/com/example/hizha/mygithubtry/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hizha.mygithubtry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
